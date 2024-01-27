0db37009748531ce810f321f64db71ee64a110a4
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2024-01-27 19:43:17 +0200

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -6,11 +6,13 @@ const apiConfig = {
   w200Image: (imgPath) => `https://image.tmdb.org/t/p/w200/${imgPath}`,
   w185Image: (imgPath) => `https://image.tmdb.org/t/p/w185/${imgPath}`,
   embedMovie: (id) => `https://vidsrc.xyz/embed/movie?tmdb=${id}`,
-  embedTV: (id, season, episode) => `https://vidsrc.xyz/embed/tv?tmdb=${id}&season=${season}&episode=${episode}`,
+  embedTV: (id) => `https://vidsrc.xyz/embed/tv?tmdb=${id}&season=1&episode=1`,
   // embedMovie: (id) => `https://2embed.org/embed/${id}`,
   // embedEpisode: (id, season, episode) =>
   // https://vidsrc.xyz/embed/movie?tmdb=385687/
   // https://vidsrc.xyz/embed/tv?tmdb=1399&season=1&episode=1
-  //   `https://2embed.org/embed/series?tmdb=${id}&sea=${season}&epi=${episode}`,
+  // https://2embed.org/embed/series?tmdb=${id}&sea=${season}&epi=${episode}
+  // embedTV: (id, season, episode) => `https://vidsrc.xyz/embed/tv?tmdb=${id}&season=${season}&episode=${episode}`,
+  // https://vidsrc.xyz/embed/tv?tmdb=1399&season=1&episode=1
 };
 export default apiConfig;
